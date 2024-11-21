a69acd83341d84b68bdcb4f4d0c317ecfbde4bf6
irajesicajanuaris <jjiraaa1@gmail.com>
2024-11-22 00:49:50 +0700

    menambahkan object mata kuliah

---
+++ app/src/main/java/com/example/navigationcompose/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.navigationcompose.data
+
+object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
+}
