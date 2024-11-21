85d52485947bc92a8aa194ff4c4a7ea65d85ad9f
irajesicajanuaris <jjiraaa1@gmail.com>
2024-11-22 03:03:12 +0700

    menambahkan Box untuk memisahkan konten dengan Header

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/MahasiswaFormView.kt
@@ -2,12 +2,14 @@ package com.example.navigationcompose.ui.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -78,4 +80,15 @@ fun MahasiswaFormView(
                 )
             }
         }
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+        Box(
+            modifier = Modifier.background(
+                color = Color.White,
+                shape = RoundedCornerShape(
+                    topEnd = 15.dp,
+                    topStart = 15.dp
+                )
+            )
+                .fillMaxSize()
+        )
 }
