69711249d6282d2fa85b9a3e06a19bed695851f9
irajesicajanuaris <jjiraaa1@gmail.com>
2024-11-22 02:46:05 +0700

    menambahkan variabel nama, nim, email

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/MahasiswaFormView.kt
@@ -1,9 +1,24 @@
 package com.example.navigationcompose.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun MahasiswaFormView(
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
-){}
+){ var nama by remember {
+    mutableStateOf("")
+}
+    var nim by remember{
+        mutableStateOf("")
+    }
+    var email by remember{
+        mutableStateOf("")
+    }
+    val listData: MutableList<String> =
+        mutableListOf(nim, nama, email)
+}
