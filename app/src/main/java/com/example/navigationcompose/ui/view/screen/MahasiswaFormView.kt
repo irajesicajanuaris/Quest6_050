975777b9500064b72e8c00536b89c439ca295946
irajesicajanuaris <jjiraaa1@gmail.com>
2024-11-22 02:41:10 +0700

    membut fungsi MahasiswaFromView

---
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/MahasiswaFormView.kt
@@ -0,0 +1,9 @@
+package com.example.navigationcompose.ui.view.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MahasiswaFormView(
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){}
