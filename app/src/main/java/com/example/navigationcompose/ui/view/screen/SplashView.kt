9484911e0f055e3b2ae8b26a47209ecdc345f66d
irajesicajanuaris <jjiraaa1@gmail.com>
2024-11-22 10:55:42 +0700

    menambahkan column untuk mengatur tata letak

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/SplashView.kt
@@ -1,8 +1,26 @@
 package com.example.navigationcompose.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.navigationcompose.R
 
 @Composable
 fun SplashView(
     onMulaiButton: () -> Unit,
-){}
+){
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .background(
+            color = colorResource(
+                id = R.color.primary
+            )
+        ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){}
