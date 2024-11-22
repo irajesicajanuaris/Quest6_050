11131255fa2a4826e2977f40cdfd6da625d7ed1e
irajesicajanuaris <jjiraaa1@gmail.com>
2024-11-22 11:05:25 +0700

    menambahkan fungsi

--- app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -11,3 +11,18 @@ class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
 
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(Matakuliah = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String){
+        _krsState.update{stateKelas -> stateKelas.copy(Kelas = kelasPilihan)}
+    }
+
+    fun saveDataKRS(Is: MutableList<String>){
+        _krsState.update { status -> status.copy(
+            Matakuliah = Is[0],
+            Kelas = Is[1]
+        ) }
+    }
+}
