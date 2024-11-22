20693794cdfc69f65f26395472cc2c789747d56f
irajesicajanuaris <jjiraaa1@gmail.com>
2024-11-22 11:09:38 +0700

    menambahkan exposedDropdownMenu untuk menampilkan opsi

--- app/src/main/java/com/example/navigationcompose/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/navigationcompose/ui/widget/DynamicSelectedField.kt
@@ -23,4 +23,39 @@ fun DynamicSelectTextField(
     label: String,
     onValueChangedEvent: (String) -> Unit,
     modifier: Modifier = Modifier
-){}
+){
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded},
+        modifier = Modifier
+    ){
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedValue,
+            onValueChange = {},
+            label = { Text(text = label) },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+            colors = OutlinedTextFieldDefaults.colors(),
+            modifier = Modifier.menuAnchor().fillMaxWidth()
+        )
+
+        ExposedDropdownMenu(expanded = expanded, onDismissRequest = {expanded = false}) {
+            options.forEach { option: String ->
+                DropdownMenuItem(
+                    text = { Text(text = option) },
+                    onClick = {
+                        expanded = false
+                        onValueChangedEvent(option)
+                    }
+                )
+            }
+        }
+    }
+}
